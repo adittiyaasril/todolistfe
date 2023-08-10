f703c26d7855d8513f2063505db2dfc504ef89c1
adittiyaasril <adittiyaasril369@gmail.com>
2023-08-10 19:19:16 +0700

    vercel

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -12,7 +12,7 @@ export const LoginForm = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:8000/api/v1/user/login",
+        "https://todolistbe.vercel.app/api/v1/user/login",
         { email, password }
       );
 
